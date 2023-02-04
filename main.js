ae45a9b7e66d7fb06818ee8808cdbc70931d26b1
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-04 19:09:10 +0900

    JavaScript初級 課題1-2

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ let number = 10;
 console.log(number * 3);
 
 console.log(`${name}くんは${number}月生まれです`);
+
+//課題 問1
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
